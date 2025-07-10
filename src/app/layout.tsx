64f671954f9cc3efd6e718354698e32f7ab291a4
maleneivy <68667843+maleneivy@users.noreply.github.com>
2025-07-10 12:20:12 +0200

    Add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,10 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html lang="en" className="h-full">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
+      >
         <Header />
-        <main>{children}</main>
+        <main className="flex-1 p-4">{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full px-4 py-2 shadow-md text-center text-sm bg-green-100">
+      © Malene Ivy Olsen {new Date().getFullYear()}
+    </footer>
+  );
+}
