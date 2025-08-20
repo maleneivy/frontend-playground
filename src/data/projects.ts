6691152e7cb4d700d43a8809cf6f10948d393765
maleneivy <68667843+maleneivy@users.noreply.github.com>
2025-08-20 10:42:30 +0200

    add monster-builder project into data/projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,4 +4,10 @@ export const projects = [
     path: '/recipe-maker',
     description: 'Lag en enkel oppskrift med grupperte ingredienser og bilde, og skriv ut.',
   },
+  {
+    title: 'Bygg ditt monster',
+    path: '/monster-builder',
+    description:
+      'Sett sammen et monster med valgbare kroppsdeler, farger og stil – og se resultatet i sanntid.',
+  },
 ];
