9b3b88418a575d6364c9a00a5ca698f97b3888d2
maleneivy <68667843+maleneivy@users.noreply.github.com>
2025-08-25 21:16:34 +0200

    change favicon and add it as a logo in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="w-full px-4 py-3 shadow-md flex items-center justify-between">
-      <div>Frontend-projects</div>
+    <header className="w-full px-4 py-3 shadow-md flex items-center justify-between bg-green-100">
+      <div className="flex items-center">
+        <Image src="/frontend-playground.png" alt="Logo" width={50} height={50} />
+      </div>
       <div className="flex gap-4">
         <Link href="/">Home</Link>
         <a
           href="https://github.com/maleneivy/frontend-playground"
           target="_blank"
-          rel="noopener norefferrer"
+          rel="noopener noreferrer"
         >
           Github-repo
         </a>
